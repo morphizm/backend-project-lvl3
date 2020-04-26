43a06b880d765f5e9bcb3c9790ce40f8eb093d52
Vasilek <morphizm@ya.ru>
2020-04-26 19:56:35 +0300

    step 2, tests, loading resourses from apge

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,28 +3,25 @@ import nock from 'nock';
 import os from 'os';
 import path from 'path';
 import { promises as fs } from 'fs';
-// import _ from 'lodash';
+import _ from 'lodash';
 import pageLoader from '../src';
 
 
-// const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+const getFixturePath = (name) => path.join(__dirname, '__fixtures__', name);
 
 let dest;
 
-// let c = console.log;
+// const c = console.log;
 
 beforeEach(async () => {
   dest = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader'));
+  await fs.unlink(dest).catch(_.noop);
 });
 
-afterAll(async () => {
-  await fs.rmdir(dest, { recursive: true });
-});
 
 describe('directory contains fileName', () => {
   test('simple url', async () => {
     const { href, pathname } = new URL('https://ru.hexlet.io/courses');
-    // const html = getFixturePath('hexlet-courses');
 
     const expectedFileName = 'ru-hexlet-io-courses.html';
 
@@ -51,4 +48,53 @@ describe('directory contains fileName', () => {
 
     expect(destFiles).toContain(expectedFileName);
   });
+});
+
+test('loading css, js, img', async () => {
+  const testHtmlPath = getFixturePath('test.html.txt');
+  const testHtml = await fs.readFile(testHtmlPath, 'utf-8');
+
+  const testCssPath = getFixturePath('test.css.txt');
+  const testCss = await fs.readFile(testCssPath, 'utf-8');
+
+  const testJsPath = getFixturePath('test.js.txt');
+  const testJs = await fs.readFile(testJsPath, 'utf-8');
+
+  const testImgPath = getFixturePath('test.jpg');
+  const testImg = await fs.readFile(testImgPath, 'base64');
+
+  const { href, pathname } = new URL('https://ru.hexlet.io/courses');
+
+  const expectedFileName = 'ru-hexlet-io-courses.html';
+  const expectedDirName = 'ru-hexlet-io-courses_files';
+
+  const [jsFileName, cssFileName, imgFileName] = ['assets-application.js', 'assets-css.css', 'assets-img.jpg'];
+
+  const dirPath = path.join(dest, expectedDirName);
+  nock(/hexlet/)
+    .get(pathname)
+    .reply(200, testHtml);
+
+  nock(/hexlet/)
+    .get(/.css/)
+    .reply(200, testCss);
+
+  nock(/hexlet/)
+    .get(/.js/)
+    .reply(200, testJs);
+
+  nock(/hexlet/)
+    .get(/.img/)
+    .reply(200, testImg);
+
+  await pageLoader(href, dest);
+  const destFiles = await fs.readdir(dest);
+  const pageFiles = await fs.readdir(dirPath);
+
+  expect(destFiles).toContain(expectedFileName);
+  expect(destFiles).toContain(expectedDirName);
+
+  expect(pageFiles).toContain(jsFileName);
+  expect(pageFiles).toContain(cssFileName);
+  expect(pageFiles).toContain(imgFileName);
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,82 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 import axios from 'axios';
 import _ from 'lodash';
-import { promises as fs } from 'fs';
+import cheerio from 'cheerio';
+
+const makeUrls = (base, ...items) => {
+  const flatItems = _.flatten(items);
+
+  return flatItems.map((itempath) => new URL(itempath, base).href);
+};
+
+let c = console.log;
+c = _.noop;
+c(_.noop);
+
+const downoloadFilesContent = (urls) => {
+  const contents = urls.map((url) => axios.get(url).then(({ data }) => ({ data, url })));
+  return Promise.all(contents);
+};
+
+const getEncoding = (fileName) => {
+  const utf8 = 'utf-8';
+  const base64 = 'base64';
+  switch (fileName) {
+    case 'css':
+      return utf8;
+    case 'js':
+      return utf8;
+    default:
+      return base64;
+  }
+};
+
+const dashPath = (pathname) => _.replace(pathname, /[^A-Za-z\d]/g, '-');
+
+const makeFiles = (dirpath, items) => {
+  const result = items.map((item) => {
+    const { data, url } = item;
+    const { pathname } = new URL(url);
+    const itemExtname = path.extname(pathname);
+    const itemPathWithoutFirstSlash = pathname.slice(1);
+    const itemPathWithoutExtname = _.replace(itemPathWithoutFirstSlash, new RegExp(`${itemExtname}$`), '');
+
+    const dashedPath = dashPath(itemPathWithoutExtname);
+    const itemName = `${dashedPath}${itemExtname}`;
+
+    const itemPath = path.join(dirpath, itemName);
+    return fs.appendFile(itemPath, data, getEncoding(itemExtname.slice(1)));
+  });
+  return Promise.all(result);
+};
 
 const pageLoader = (pageUrl, outputDirectory = process.cwd()) => {
   const urlWithoutHttp = _.replace(pageUrl, /http:\/\/|https:\/\//, '');
-  const dashedUrl = _.replace(urlWithoutHttp, /[^A-Za-z\d]/g, '-');
+  const dashedName = dashPath(urlWithoutHttp);
 
-  return axios.get(pageUrl)
-    .then(() => fs.appendFile(`${outputDirectory}/${dashedUrl}.html`, '', 'utf-8'));
+  const htmlFileName = `${dashedName}.html`;
+  const contentsDirName = `${dashedName}_files`;
+
+  const outputHtmlPath = path.join(outputDirectory, htmlFileName);
+  const contentsDirPath = path.join(outputDirectory, contentsDirName);
+
+  const result = axios.get(pageUrl)
+    .then(({ data }) => data)
+    .then((data) => {
+      const $ = cheerio.load(data);
+      const linksElementsRefs = $('link').map((i, el) => $(el).attr('href')).get();
+      const scriptElementsRefs = $('script').map((i, el) => $(el).attr('src')).get();
+      const imgElementsRefs = $('img').map((i, el) => $(el).attr('src')).get();
+
+      const urls = makeUrls(pageUrl, linksElementsRefs, scriptElementsRefs, imgElementsRefs);
+
+      return fs.appendFile(outputHtmlPath, data, 'utf-8')
+        .then(() => fs.mkdir(contentsDirPath))
+        .then(() => downoloadFilesContent(urls));
+    })
+    .then((values) => makeFiles(contentsDirPath, values));
+  return result;
 };
 
 export default pageLoader;
