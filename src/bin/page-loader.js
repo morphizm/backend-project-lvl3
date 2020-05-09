4c33c6652d399e30519aaa8aa6488ac0c3abda84
Vasilek <morphizm@ya.ru>
2020-05-09 18:19:05 +0300

    change version of package

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -5,7 +5,7 @@ import pageLoader from '..';
 const program = new Command();
 
 program
-  .version('0.0.13')
+  .version('0.0.14')
   .description('Downoload files from web page')
   .option('--output <directory>', 'Output directory', process.cwd())
   .arguments('<pageUrl>')
